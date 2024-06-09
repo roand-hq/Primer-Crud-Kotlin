caa77bec241849f5f8c63ead9b107fc63a28b450
Rodrigo Hurtado <20220224@ricaldone.edu.sv>
2024-06-08 18:23:19 -0600

    Perfil terminado, y con él finalmente todo el proyecto

--- app/src/main/java/rodrigo/hurtado/appcrud/ui_perfil.kt
+++ app/src/main/java/rodrigo/hurtado/appcrud/ui_perfil.kt
@@ -1,10 +1,12 @@
 package rodrigo.hurtado.appcrud
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
 import android.widget.TextView
 
 
@@ -21,11 +23,14 @@ class ui_perfil : Fragment() {
 
         val correo = arguments?.getString("correo")
         val clave = arguments?.getString("clave")
+        val txtCorreoPerfil = vista.findViewById<TextView>(R.id.txtCorreoPerfil)
+        val btnCierreSesion = vista.findViewById<Button>(R.id.btnCierreSesion)
+        txtCorreoPerfil.text = correo
 
-        val txtPerfilCorreo = vista.findViewById<TextView>(R.id.txtPerfilCorreo)
-        val txtPerfilClave = vista.findViewById<TextView>(R.id.txtPerfilClave)
-
-
+        btnCierreSesion.setOnClickListener {
+            val intento = Intent(requireContext(), MainActivity::class.java)
+            startActivity(intento)
+        }
         return vista
     }
 
